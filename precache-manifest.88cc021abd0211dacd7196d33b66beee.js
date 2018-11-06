ce2cf8008d794c2cfa19bb94357b171dc18c13a1
Jake Hassel <jake.k.hassel@gmail.com>
2018-11-05 22:49:18 -0500

    Updates

---
+++ precache-manifest.88cc021abd0211dacd7196d33b66beee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/rate-dogs/robots.txt"
+  },
+  {
+    "revision": "4e6fb9dbcc0a2f8ca3c7",
+    "url": "/rate-dogs/js/chunk-vendors.f96e7f11.js"
+  },
+  {
+    "revision": "f33b7b9b65619dbc5d6c",
+    "url": "/rate-dogs/js/app.e8d43d27.js"
+  },
+  {
+    "revision": "9b046755515525ce7d3863252b63972f",
+    "url": "/rate-dogs/index.html"
+  },
+  {
+    "revision": "6affab38859a2f4dd4fab3bec40d02e2",
+    "url": "/rate-dogs/img/materialdesignicons-webfont.6affab38.svg"
+  },
+  {
+    "revision": "6536e4067e72c89cd3f1530d83e9888c",
+    "url": "/rate-dogs/fonts/materialdesignicons-webfont.6536e406.woff2"
+  },
+  {
+    "revision": "5d43a6f507961589ab369ccc6f6d301b",
+    "url": "/rate-dogs/fonts/materialdesignicons-webfont.5d43a6f5.woff"
+  },
+  {
+    "revision": "41da82f6e715b98dc05e12686231e831",
+    "url": "/rate-dogs/fonts/materialdesignicons-webfont.41da82f6.eot"
+  },
+  {
+    "revision": "023db9122f66b7d693bc52bcdf09e6b3",
+    "url": "/rate-dogs/fonts/materialdesignicons-webfont.023db912.ttf"
+  },
+  {
+    "revision": "4e6fb9dbcc0a2f8ca3c7",
+    "url": "/rate-dogs/css/chunk-vendors.7f32d491.css"
+  }
+];
