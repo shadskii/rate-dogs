c65cce9f6737a1fe2319f523535f3093233539b0
Jake Hassel <jake.k.hassel@gmail.com>
2018-11-05 20:31:50 -0500

    Dog rating for one dog added

---
+++ src/components/DogCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-img 
+      aspect-ratio="1"
+      :src="url"
+    />
+    <div class="text-xs-center mt-2">
+      <v-rating v-model="rating" />
+    </div>
+  </v-card>
+</template>
+<script>
+export default {
+    props:{
+        url: {
+            type: String,
+            default: ''
+        }
+    },
+    data(){
+        return {
+            rating: 1
+        }
+    }
+}
+</script>
+
---
+++ src/components/DogContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex 
+        v-for="dog in dogs" 
+        :key="`flex-${dog.url}`"
+      >
+        <dog-card 
+          :key="dog.url" 
+          :url="dog.url"
+        />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+import DogCard from './DogCard.vue';
+export default {
+    components:{
+        DogCard
+    },
+    data(){
+        return{
+            dogs: [
+                {
+                    url: "https://images.dog.ceo/breeds/pembroke/n02113023_12248.jpg"
+                }
+            ]
+        }
+    }
+    
+}
+</script>
