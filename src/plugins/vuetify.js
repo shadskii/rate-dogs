4ef8a8b5b5a562a318344ebf008dcba5e5c3161d
Jake Hassel <jake.k.hassel@gmail.com>
2018-11-05 22:35:36 -0500

    mdi icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/src/stylus/app.styl';
 import colors from 'vuetify/es5/util/colors';
-
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify, {
-  iconfont: 'md',
+  iconfont: 'mdi',
   theme: {
     primary: colors.blue,
     secondary: colors.grey.darken1,
